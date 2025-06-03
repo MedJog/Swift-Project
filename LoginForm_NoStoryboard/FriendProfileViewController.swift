eb12aa6cb3e16eb5fdc095887a2b299db92f27b8
Svetlana <medjog@gmail.com>
2025-06-04 00:24:20 +0300

    Homework 6 added transition to friend's profile

--- LoginForm_NoStoryboard/FriendProfileViewController.swift
+++ LoginForm_NoStoryboard/FriendProfileViewController.swift
@@ -1,31 +1,45 @@
 import UIKit
 
-final class FriendProfileViewController: UIViewController {
+class FriendProfileViewController: UIViewController {
 
-    var friend: Friend?
+    private let friend: Friend
 
-    private let nameLabel = UILabel()
+    init(friend: Friend) {
+        self.friend = friend
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        title = "Профиль друга"
+        title = "\(friend.firstName) \(friend.lastName)"
         setupUI()
     }
 
     private func setupUI() {
+        let nameLabel = UILabel()
+        nameLabel.text = "\(friend.firstName) \(friend.lastName)"
+        nameLabel.font = .systemFont(ofSize: 24, weight: .bold)
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.font = UIFont.systemFont(ofSize: 22, weight: .bold)
-        nameLabel.textAlignment = .center
+
+        let onlineLabel = UILabel()
+        onlineLabel.text = friend.online == 1 ? "Онлайн" : "Оффлайн"
+        onlineLabel.textColor = friend.online == 1 ? .systemGreen : .systemGray
+        onlineLabel.translatesAutoresizingMaskIntoConstraints = false
+
         view.addSubview(nameLabel)
+        view.addSubview(onlineLabel)
 
         NSLayoutConstraint.activate([
             nameLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nameLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
+            nameLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
 
-        if let friend = friend {
-            nameLabel.text = "\(friend.firstName) \(friend.lastName)"
-        }
+            onlineLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            onlineLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20)
+        ])
     }
 }
